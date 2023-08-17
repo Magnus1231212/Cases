e57ecf7189c38d64bf57488d9da0d69ef74b5c31
Magnus1231212 <nolfimagnus@outlook.dk>
2023-08-17 09:55:14 +0200

    Add project files.

---
+++ Cases/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program starting...");
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+
+    static class Fodbold
+    {
+
+    }
+
+    static class Dansekonkurrence
+    {
+
+    }
+
+    static class Password
+    {
+
+    }
+
+}
